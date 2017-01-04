6ea4c809414f1b92f295e9a48d03a5b2dd6a0122
composed2016 <tolick.v2016@yandex.ru>
2017-01-04 16:49:51 +0300

    заменил ctime на localtime

--- test.py
+++ test.py
@@ -3,13 +3,13 @@
 import time
 
 print(time.ctime())
-time_list  = time.ctime()
-hours = int(time_list[11]+time_list[12])
+time_list=time.ctime()
+hours=int(time_list[11] + time_list[12])
 print(hours)
-if hours <= 12:
+if 5 >= hours <= 12:
     print('утро')
- elif 13=> hours => 20:
+ elif 13 >= hours >= 20:
      print('день')
-else
+else:
    print('вечер')
 
