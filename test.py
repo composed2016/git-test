5b58f880648f4a2000ebfae7ef30a92e2ba30fa1
composed2016 <tolick.v2016@yandex.ru>
2017-01-04 18:14:12 +0300

    добавлено меню и работает информация о системе

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import time
+import os
 
 print(time.ctime())
 name = input('Введите свой логин: ' )
@@ -15,4 +16,30 @@ elif 18 < int(hours) < 22:
     print("Доброго вечера,",name)
 else:
     print("Доброй ночи,",name)
-
+print("что бы вы хотели сделать?")
+print("[1] вывести информацию о системе ")
+print("[2] ")
+print("[3] ")
+print("[4] ")
+print("[5] ")
+print("[9] выход ")
+do = ''
+while do != 9:
+do = int(input("выберите пункт:" ))
+if do == 1:
+    print("Данные вашей системы",os.uname())
+    print ("количество просессоров", os.cpu_count())
+    print ("вы находитесь",os.getcwd())
+    print ("вы вошли в систему под логином",os.getlogin(), os.getuid())
+elif do == 2:
+    print ("2")
+elif do == 3:
+    print ("3")
+elif do == 4:
+    print ("4")
+elif do == 5:
+    print ("5")
+else:
+    pass
+        
+        
