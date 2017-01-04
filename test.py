1ef10c1e2d7b8230755d63569cf0fc21776adb6a
composed2016 <tolick.v2016@yandex.ru>
2017-01-04 13:59:48 +0300

    пробую выводить время суток

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
 #/usr/bin/python3
-print ('hello world')
+# coding: utf-8
+import time
+
+print(time.ctime())
+time_list  = time.ctime()
+hours = int(time_list[11]+time_list[12])
+print(hours)
+if hours <= 12:
+    print('утро')
+ elif 13=> hours => 20:
+     print('день')
+else
+   print('вечер')
+
