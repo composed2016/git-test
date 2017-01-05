3e714bf154985e52a200ccde21e31718fe1865b1
composed2016 <tolick.v2016@yandex.ru>
2017-01-05 22:11:14 +0300

    выводит информацию о компьютере.

--- test.py
+++ test.py
@@ -5,17 +5,17 @@ import time
 import os
 
 print(time.ctime())
-name = input('Введите свой логин: ' )
+name = input('Введите свой логин: ')
 time_list = time.localtime()
 hours = time_list[3]
-if 5 < int(hours) < 12:
-    print("Доброго утра,",name)
-elif 12 < int(hours) < 18:
-    print("Доброго деня,",name)
-elif 18 < int(hours) < 22:
-    print("Доброго вечера,",name)
+if 5 < int(hours) <= 12:
+    print("Доброго утра,", name)
+elif 12 < int(hours) <= 18:
+    print("Доброго деня,", name)
+elif 18 < int(hours) <= 22:
+    print("Доброго вечера,", name)
 else:
-    print("Доброй ночи,",name)
+    print("Доброй ночи,", name)
 print("что бы вы хотели сделать?")
 print("[1] вывести информацию о системе ")
 print("[2] ")
@@ -25,21 +25,21 @@ print("[5] ")
 print("[9] выход ")
 do = ''
 while do != 9:
-do = int(input("выберите пункт:" ))
-if do == 1:
-    print("Данные вашей системы",os.uname())
-    print ("количество просессоров", os.cpu_count())
-    print ("вы находитесь",os.getcwd())
-    print ("вы вошли в систему под логином",os.getlogin(), os.getuid())
-elif do == 2:
-    print ("2")
-elif do == 3:
-    print ("3")
-elif do == 4:
-    print ("4")
-elif do == 5:
-    print ("5")
-else:
-    pass
-        
-        
+    do = int(input("выберите пункт:"))
+
+    if do == 1:
+        print("Данные вашей системы", os.uname())
+        print("количество просессоров", os.cpu_count())
+        print("вы находитесь", os.getcwd())
+        print("вы вошли в систему под логином", os.getlogin())
+        print('')
+    elif do == 2:
+        print("2")
+    elif do == 3:
+        print("3")
+    elif do == 4:
+        print("4")
+    elif do == 5:
+        print("5")
+    else:
+        pass
